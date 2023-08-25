5b6c93da79e352ef3919ba6a4cbdd61c42842191
Sahil-987 <sahil83765@gmail.com>
2023-08-25 11:13:00 +0530

    css-cascade

---
+++ Web Development Projects/rough/files-code/filenames.py
@@ -0,0 +1,20 @@
+import os
+
+print("Hello World")
+# for i in os.listdir("../../Web-Development-Bootcamp/"):
+#     print(i)
+
+
+print(os.listdir("../main/Web Development Projects/"))
+print(os.listdir("../main/Web Development Projects/rough/files-code/"))
+
+
+print()
+print("new approach")
+print()
+
+
+
+
+
+
