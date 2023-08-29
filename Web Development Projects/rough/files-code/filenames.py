1a402386db794b0cb25dabe3d84f1dbf0ff2160f
Sahil-987 <sahil83765@gmail.com>
2023-08-29 14:43:21 +0530

    css-positioning : static, relative, absolute, fixed

--- Web Development Projects/rough/files-code/filenames.py
+++ Web Development Projects/rough/files-code/filenames.py
@@ -9,9 +9,21 @@ print(os.listdir("../main/Web Development Projects/"))
 print(os.listdir("../main/Web Development Projects/rough/files-code/"))
 
 
+
 print()
-print("new approach")
+print("******** New Approach ********")
 print()
+
+
+# Get the directory of the script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Set the working directory to the script's directory
+os.chdir(script_dir)
+
+print(os.listdir("../files-code/"))
+print(os.listdir("../../rough/"))
+print(os.listdir("../../../Web Development Projects"))
 
 
 
